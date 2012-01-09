e4bddd03eb5f36f722d33189a894ae5ed507edca
Sirix <Sirix25@gmail.com>
2012-01-09 22:38:58 +0200

    Improvements in AuthorizedToRolesAttribute class

--- NProject.Source/NProject/Infrastructure/AuthorizedToRolesAttribute.cs
+++ NProject.Source/NProject/Infrastructure/AuthorizedToRolesAttribute.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NProject.BLL;
+using NProject.Helpers;
 using NProject.Models.Domain;
-using NProject.Models.Infrastructure;
 
 namespace NProject.Infrastructure
 {
@@ -12,25 +11,42 @@ namespace NProject.Infrastructure
     {
         public UserRole AllowedRoles { get; set; }
 
+        public AuthorizedToRolesAttribute()
+        {
+            AllowedRoles = UserRole.Unspecified;
+        }
+
+        public AuthorizedToRolesAttribute(UserRole roles)
+        {
+            AllowedRoles = roles;
+        }
+
         public override void OnAuthorization(AuthorizationContext filterContext)
         {
             base.OnAuthorization(filterContext);
 
             if (filterContext.Result is HttpUnauthorizedResult)
             {
-                filterContext.Result = new ViewResult { ViewName = "AccessError" };
+                filterContext.Result = new ViewResult {ViewName = "AccessError"};
             }
         }
+
         protected override bool AuthorizeCore(HttpContextBase httpContext)
         {
-            if (!httpContext.User.Identity.IsAuthenticated) return false;
+            if (httpContext == null)
+                throw new ArgumentNullException("httpContext");
 
-            string userName = httpContext.User.Identity.Name;
-            using (var dba = new DbAccessPoint())
-            {
-                var u = dba.Users.First(i => i.Username == userName);
-                return (AllowedRoles.HasFlag(u.Role));
-            }
+            //if request is from a guest
+            if (!httpContext.Request.IsAuthenticated) return false;
+
+            //if we don't have any specified roles, we allow any role
+            if (AllowedRoles == UserRole.Unspecified) return true;
+
+            //if we are here, we have authentificated user, so we can use SessionStorage
+            int userId = SessionStorage.User.Id;
+            var us = new UserService();
+
+            return AllowedRoles.HasFlag(us.GetUser(userId).Role);
         }
     }
 }
